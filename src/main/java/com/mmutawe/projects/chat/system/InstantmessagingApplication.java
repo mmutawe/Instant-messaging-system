7a5261b27066935e7e011604e53e06787b1ee6d4
mmutawe <mahmoudmutawe@gmail.com>
2022-11-17 22:08:12 -0600

    Initial commit

---
+++ src/main/java/com/mmutawe/projects/chat/system/InstantmessagingApplication.java
@@ -0,0 +1,13 @@
+package com.mmutawe.projects.chat.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantmessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantmessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmutawe/projects/chat/system/InstantmessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmutawe.projects.chat.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantmessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
